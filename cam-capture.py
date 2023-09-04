d7493d4a3f7d944428173e04dc15ff0ce48f0d1f
glennpck <glennchia43@gmail.com>
2023-09-04 10:48:51 +0800

    cvzone-hand-crop

--- cam-capture.py
+++ cam-capture.py
@@ -8,6 +8,12 @@ detector = HandDetector(maxHands=1)
 while True:
     success, img = capture.read()
     hands, img = detector.findHands(img)
+    if hands:
+        hand = hands[0]
+        x, y, w, h = hand['bbox']
+        crop = img[y:y+h, x:x+w]
+        cv2.imshow("ImageCrop", crop)
+
     cv2.imshow("Image", img)
     cv2.waitKey(1)
 
